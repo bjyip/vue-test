0c5ac37de4c3d3a4649ae0e4b63ee5ee7364f639
yebaojia <yebaojia@aiyunxiao.com>
2019-09-02 18:07:58 +0800

    init

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,58 +1,129 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
-  </div>
+    <div class="hello">
+        <el-table :data="banjis" border style="width: 100%">
+            <el-table-column prop="banji" label="班级" width="180"></el-table-column>
+            <el-table-column label="班主任" width="180">
+                <template slot-scope="prop">
+                    <el-row v-for="(item, index) in prop.row.headmaster" :key="index" class="button-input">
+                        <el-autocomplete
+                            v-model="item.name"
+                            placeholder="请输入内容"
+                            :trigger-on-focus="item.editable"
+                            :fetch-suggestions="fetchSuggestions"
+                            :class="{'is-editable': item.editable}"
+                            @dblclick.native="dblclick($event, item)"
+                            @blur="blur($event, item)"
+                            @keyup.native.ctrl.67="ctrlC($event, item)"
+                        ></el-autocomplete>
+                    </el-row>
+                    <el-row>
+                        <el-autocomplete
+                            v-model="currentValue"
+                            placeholder="请输入内容"
+                            :fetch-suggestions="fetchSuggestions"
+                            @keyup.native.ctrl.86="ctrlV($event, prop.$index, 'headmaster')"
+                        ></el-autocomplete>
+                    </el-row>
+                </template>
+            </el-table-column>
+            <el-table-column label="语文" width="180">
+                <template slot-scope="prop">
+                    <el-row v-for="(item, index) in prop.row.chinese" :key="index" class="button-input">
+                        <el-autocomplete
+                            v-model="item.name"
+                            placeholder="请输入内容"
+                            :class="{'is-editable': item.editable}"
+                            :trigger-on-focus="item.editable"
+                            :fetch-suggestions="fetchSuggestions"
+                            @dblclick.native="dblclick($event, item)"
+                            @blur="blur($event, item)"
+                            @keyup.native.ctrl.67="ctrlC($event, item)"
+                        ></el-autocomplete>
+                    </el-row>
+                    <el-row>
+                        <el-autocomplete
+                            v-model="currentValue"
+                            placeholder="请输入内容"
+                            :fetch-suggestions="fetchSuggestions"
+                            @keyup.native.ctrl.86="ctrlV($event, prop.$index, 'chinese')"
+                        ></el-autocomplete>
+                    </el-row>
+                </template>
+            </el-table-column>
+        </el-table>
+    </div>
 </template>
 
 <script>
+import _data from '../data';
+import { setTimeout } from 'timers';
+const _ = require('lodash');
 export default {
-  name: 'HelloWorld',
-  props: {
-    msg: String
-  }
-}
+    name: "HelloWorld",
+    data() {
+        return Object.assign(_data, {
+            currentValue: '',
+            copyValue: {},
+        })
+    },
+    mounted() {
+        this.setBtnInput();
+    },
+    methods: {
+        fetchSuggestions(queryString, callback) {
+            if (!queryString) {
+                return;
+            }
+            setTimeout(() => {
+                callback([
+                    {
+                        value: queryString + 1,
+                    },
+                    {
+                        value: queryString + 2,
+                    }
+                ])
+            }, 500);
+        },
+        ctrlC(e, item) {
+            console.log('ctrlC')
+            if (e.target.getAttribute('type') === 'button') {
+                console.log(item)
+                this.copyValue = _.cloneDeep(item);
+            }
+        },
+        ctrlV(e, index, type) {
+            console.log('ctrlV')
+            console.log(e, this.copyValue, index, type)
+            this.banjis[index][type].push(this.copyValue);
+            this.setBtnInput();
+        },
+        blur(e) {
+            console.log('blur')
+            e.target.setAttribute('type', 'button');
+            this.disabled = true;
+        },
+        dblclick(e) {
+            console.log('dblclick')
+            this.disabled = false;
+            e.target.setAttribute('type', 'text');
+        },
+        setBtnInput() {
+            this.$nextTick(() => {
+                const autocompleteNode = document.querySelectorAll('.button-input .el-autocomplete');
+                autocompleteNode.forEach(input => {
+                    const inputNode = input.querySelector('input');
+                    inputNode.setAttribute('type', 'button');
+                });
+            });
+        }
+    },
+};
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-h3 {
-  margin: 40px 0 0;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
-}
+    input {
+        width: 80px;
+    }
 </style>
---
+++ src/data.js
@@ -0,0 +1,244 @@
+export default{
+    banjis: [
+        {
+            banji: "1班",
+            headmaster: [
+                {
+                    name: "翻云镇",
+                    iphone: 15692402222,
+                    editable: false
+                }
+            ],
+
+            chinese: [
+                {
+                    name: "张三",
+                    iphone: 15692402222,
+                    editable: false
+                }
+            ],
+            math: [
+                {
+                    name: "欧阳",
+                    iphone: 15692402222,
+                    editable: false
+                }
+            ],
+            english: [
+                {
+                    name: "网易",
+                    iphone: 15692402222,
+                    editable: false
+                }
+            ],
+            physics: [
+                {
+                    name: "张三",
+                    iphone: 15692402222,
+                    editable: false
+                }
+            ],
+            chemistry: [
+                {
+                    name: "里斯",
+                    iphone: 15692402222,
+                    editable: false
+                }
+            ],
+            biology: [
+                {
+                    name: "王五",
+                    iphone: 15692402222,
+                    editable: false
+                }
+            ],
+            history: [
+                {
+                    name: "雪梨",
+                    iphone: 15692402222,
+                    editable: false
+                }
+            ],
+            geography: [
+                {
+                    name: "苹果",
+                    iphone: 15692402222,
+                    editable: false
+                }
+            ],
+            politics: [
+                {
+                    name: "西瓜",
+                    iphone: 15692402222,
+                    editable: false
+                }
+            ]
+        },
+        {
+            banji: "2班",
+            headmaster: [
+                {
+                    name: "欧阳",
+                    iphone: 15692402222,
+                    editable: false
+                }
+            ],
+
+            chinese: [
+                {
+                    name: "六十一",
+                    iphone: 15692402222,
+                    editable: false
+                }
+            ],
+            math: [
+                {
+                    name: "欧阳",
+                    iphone: 15692402222,
+                    editable: false
+                }
+            ],
+            english: [
+                {
+                    name: "网易",
+                    iphone: 15692402222,
+                    editable: false
+                }
+            ],
+            physics: [
+                {
+                    name: "张三",
+                    iphone: 15692402222,
+                    editable: false
+                }
+            ],
+            chemistry: [
+                {
+                    name: "里斯",
+                    iphone: 15692402222,
+                    editable: false
+                }
+            ],
+            biology: [
+                {
+                    name: "王五",
+                    iphone: 15692402222,
+                    editable: false
+                }
+            ],
+            history: [
+                {
+                    name: "雪梨",
+                    iphone: 15692402222,
+                    editable: false
+                }
+            ],
+            geography: [
+                {
+                    name: "苹果",
+                    iphone: 15692402222,
+                    editable: false
+                }
+            ],
+            politics: [
+                {
+                    name: "西瓜",
+                    iphone: 15692402222,
+                    editable: false
+                }
+            ]
+        }
+    ],
+    teachingClass: {
+        语文: [
+            {
+                banji: "语文a班",
+                name: "刘事宜"
+            },
+            {
+                banji: "语文b班",
+                name: "李欣"
+            }
+        ],
+        数学: [
+            {
+                banji: "数学a班",
+                name: "刘事宜"
+            },
+            {
+                banji: "数学b班",
+                name: "李欣"
+            }
+        ],
+        英语: [
+            {
+                banji: "英语a班",
+                name: "刘事宜"
+            },
+            {
+                banji: "英语b班",
+                name: "李欣"
+            }
+        ],
+        物理: [
+            {
+                banji: "物理a班",
+                name: "刘事宜"
+            },
+            {
+                banji: "物理b班",
+                name: "李欣"
+            }
+        ],
+        化学: [
+            {
+                banji: "化学a班",
+                name: "刘事宜"
+            },
+            {
+                banji: "化学b班",
+                name: "李欣"
+            }
+        ],
+        生物: [
+            {
+                banji: "生物a班",
+                name: "刘事宜"
+            },
+            {
+                banji: "生物b班",
+                name: "李欣"
+            }
+        ],
+        历史: [
+            {
+                banji: "历史a班",
+                name: "刘事宜"
+            },
+            {
+                banji: "历史b班",
+                name: "李欣"
+            }
+        ],
+        地理: [
+            {
+                banji: "地理a班",
+                name: "刘事宜"
+            },
+            {
+                banji: "地理b班",
+                name: "李欣"
+            }
+        ],
+        政治: [
+            {
+                banji: "政治a班",
+                name: "刘事宜"
+            },
+            {
+                banji: "政治b班",
+                name: "李欣"
+            }
+        ]
+    }
+}
